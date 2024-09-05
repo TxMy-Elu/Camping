30be3c0364b6031278ac718920ff9a41eff9f513
TxMy <to.doguet@gmail.com>
2024-09-05 21:49:53 +0200

    Login page update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.camping {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires jdk.internal.vm.compiler;
 
     opens com.example.camping to javafx.fxml;
     exports com.example.camping;
