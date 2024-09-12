8fb5f5137ef9a559145a01c6f4a9321b9a243b9f
Sara <sara.grs09@gmail.com>
2024-09-12 13:19:10 +0200

    modif

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.camping {
     requires javafx.fxml;
     requires java.sql;
-    requires com.calendarfx.view;
+    requires javafx.graphics;
+    requires javafx.controls;
 
     opens com.example.camping to javafx.fxml;
     exports com.example.camping;
