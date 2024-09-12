e423056dd61ec6efb8ef6623f1aa6badaa25f16a
TxMy <to.doguet@gmail.com>
2024-09-12 12:58:22 +0200

    Test CalendarFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.camping {
-    requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.calendarfx.view;
 
     opens com.example.camping to javafx.fxml;
     exports com.example.camping;
