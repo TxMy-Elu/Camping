649ea1f38a48bd981a26c460f94c7c2a4c53e94b
Sara <sara.grs09@gmail.com>
2024-09-19 08:46:10 +0200

    Class
    Lieu

--- src/main/java/com/example/camping/Lieu.java
+++ src/main/java/com/example/camping/Lieu.java
@@ -1,5 +1,27 @@
 package com.example.camping;
 
 public class Lieu {
-    
+    private int id_lieu;
+    private String libelle;
+
+    public Lieu(int id_lieu, String libelle) {
+        this.id_lieu = id_lieu;
+        this.libelle = libelle;
+    }
+
+    public int getId_lieu() {
+        return id_lieu;
+    }
+
+    public void setId_lieu(int id_lieu) {
+        this.id_lieu = id_lieu;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 }
