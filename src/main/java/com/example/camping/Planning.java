c5a51af1ce64a8aab1eeb8372964085acd00c4a9
Sara <sara.grs09@gmail.com>
2024-09-19 11:12:44 +0200

    création Planning
    Création de la classe planning pour afficher dans la listview les animateurs et les crenaux ensemble

---
+++ src/main/java/com/example/camping/Planning.java
@@ -0,0 +1,13 @@
+package com.example.camping;
+
+public class Planning {
+    private Animateur animateur;
+    private Creneaux creneaux;
+
+    public Planning(Animateur animateur, Creneaux creneaux) {
+        this.animateur = animateur;
+        this.creneaux = creneaux;
+    }
+
+
+}
