ad0256428c8300b15132fe79e53d523527de3511
TxMy <to.doguet@gmail.com>
2024-10-17 13:31:17 +0200

    ActTest

--- src/main/java/com/example/camping/Act.java
+++ src/main/java/com/example/camping/Act.java
@@ -7,38 +7,10 @@ public class Act {
     private Animateur animateur;
     private Creneaux creneaux;
     private Animation animation;
-    private String lundi;
-    private String mardi;
-    private String mercredi;
-    private String jeudi;
-    private String vendredi;
-    private String horaires;
 
     public Act(Animateur animateur, Creneaux creneaux) {
         this.animateur = animateur;
         this.creneaux = creneaux;
-        this.lundi = "";
-        this.mardi = "";
-        this.mercredi = "";
-        this.jeudi = "";
-        this.vendredi = "";
-    }
-
-    public Act() {
-        this.horaires = "";
-        this.lundi = "";
-        this.mardi = "";
-        this.mercredi = "";
-        this.jeudi = "";
-        this.vendredi = "";
-    }
-
-    public String getHoraires() {
-        return horaires;
-    }
-
-    public void setHoraires(String horaires) {
-        this.horaires = horaires;
     }
 
     public Animateur getAnimateur() {
@@ -49,52 +21,20 @@ public class Act {
         this.animateur = animateur;
     }
 
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public void setAnimation(Animation animation) {
+        this.animation = animation;
+    }
+
     public Creneaux getCreneaux() {
         return creneaux;
     }
 
     public void setCreneaux(Creneaux creneaux) {
         this.creneaux = creneaux;
-    }
-
-    public String getLundi() {
-        return lundi;
-    }
-
-    public void setLundi(String lundi) {
-        this.lundi = lundi;
-    }
-
-    public String getMardi() {
-        return mardi;
-    }
-
-    public void setMardi(String mardi) {
-        this.mardi = mardi;
-    }
-
-    public String getMercredi() {
-        return mercredi;
-    }
-
-    public void setMercredi(String mercredi) {
-        this.mercredi = mercredi;
-    }
-
-    public String getJeudi() {
-        return jeudi;
-    }
-
-    public void setJeudi(String jeudi) {
-        this.jeudi = jeudi;
-    }
-
-    public String getVendredi() {
-        return vendredi;
-    }
-
-    public void setVendredi(String vendredi) {
-        this.vendredi = vendredi;
     }
 
     public String getId_DateHeure() {
